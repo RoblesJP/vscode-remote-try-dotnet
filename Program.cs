c0f632ba5467eb39d57b2f0a85a568b8213f1205
Juan Robles <juan.robles1417@gmail.com>
2023-02-28 02:16:53 +0000

    Commit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,31 @@
+public enum CarType
+{
+  Sedan,
+  Crossover
+}
 
+class Car
+{
+  public CarType Type;
+  public int WheelSize;
+}
 
-class Program 
+interface ISpecifyCarType
+{
+  ISpecifyWheelSize OfType(CarType type);
+}
+
+interface ISpecifyWheelSize
+{
+  IBuildCar WithWheels(int size);
+}
+
+interface IBuildCar
+{
+  public Car Build();
+}
+
+class Program
 {
   static void Main()
   {
