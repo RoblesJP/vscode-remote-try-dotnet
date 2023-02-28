342f49bc6e32d5f102ed1babe920766ea7fc941e
Juan Robles <juan.robles1417@gmail.com>
2023-02-28 23:53:23 +0000

    Stepwise Builder

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ class Car
   public int WheelSize;
 }
 
+// applying interface segregation principle
 interface ISpecifyCarType
 {
   ISpecifyWheelSize OfType(CarType type);
@@ -25,10 +26,51 @@ interface IBuildCar
   public Car Build();
 }
 
+class CarBuilder
+{
+  private class Impl : ISpecifyCarType, ISpecifyWheelSize, IBuildCar
+  {
+    private Car car = new Car();
+
+    public ISpecifyWheelSize OfType(CarType type)
+    {
+      car.Type = type;
+      return this;
+    }
+
+    public IBuildCar WithWheels(int size)
+    {
+      switch (car.Type)
+      {
+        case CarType.Crossover when size < 17 || size > 20:
+        case CarType.Sedan when size < 15 || size > 17:
+          throw new ArgumentException($"Wrong size of wheel for {car.Type}");
+      }
+
+      car.WheelSize = size;
+      return this;
+    }
+
+    public Car Build()
+    {
+      return car;
+    }
+  }
+  public static ISpecifyCarType Create()
+  {
+    return new Impl();
+  }
+}
+
 class Program
 {
   static void Main()
   {
-    Console.WriteLine("Hello World!");
+    var car = CarBuilder.Create()
+      .OfType(CarType.Sedan)
+      .WithWheels(16)
+      .Build();
+
+    Console.WriteLine(car);
   }
 }
