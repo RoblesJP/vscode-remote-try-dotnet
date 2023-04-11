e56bb2ac832418ee8adb0db93da1776d5fe4a0df
Juan Robles <juan.robles1417@gmail.com>
2023-04-11 23:58:23 +0000

    Inner Factory

--- Program.cs
+++ Program.cs
@@ -3,32 +3,39 @@ public class Point
   private double x;
   private double y;
 
-  public Point(double x, double y)
+  private Point(double x, double y)
   {
     this.x = x;
     this.y = y;
   }
+
+  public static Point Origin = new Point(0, 0);
+
+  public class Factory
+  {
+    // factory method
+    public static Point NewCartesianPoint(double x, double y)
+    {
+      return new Point(x, y);
+    }
+
+    // factory method
+    public static Point NewPolarPoint(double rho, double theta)
+    {
+      return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+    }
+  }
 }
 
-public class PointFactory
-{
-  // factory method
-  public static Point NewCartesianPoint(double x, double y)
-  {
-    return new Point(x, y);
-  }
 
-  // factory method
-  public static Point NewPolarPoint(double rho, double theta)
-  {
-    return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
-  }
-}
 
 class Program
 {
   static void Main()
   {
-    var point = PointFactory.NewCartesianPoint(3, 2);
+    var point = Point.Factory.NewCartesianPoint(3, 2);
+    Console.WriteLine(point);
+
+    var origin = Point.Origin;
   }
 }
